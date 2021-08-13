a9b4ddded6397e46d29bd1a126a4ec31f7174e27
Jacob Shumer <jacob.shumer@ascte.org>
2021-08-12 22:10:05 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# from functions.py import *
+
+def main(args):
+    print("Use + for addition, Use - for subtraction, Use * for multiplication, Use / for division.")
+    problem = input("What is the math problem to solve(one-step)? ")
+    valid = False
+    functions = {
+        1: "+",
+        2: "-",
+        3: "*",
+        4: "/"
+    }
+
+    for each in functions:
+        if problem.contains(each):
+            valid = True
+
+    if not valid:
+        print("FAIL")
+        exit()
+
+    print("PASS")
+
+#
+# class Solve:
+
+
+#    def FindOperation():
