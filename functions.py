82e78017beccbac24de3f2398dc300ec92a4c2b7
Kano <kano@pclip.dev>
2021-08-12 21:04:40 -0700

    Complete (Debug dialog still present)

---
+++ functions.py
@@ -0,0 +1,14 @@
+def addition(integers):
+    return int(integers[0]) + int(integers[1])
+
+
+def subtraction(integers):
+    return int(integers[0]) + int(integers[1])
+
+
+def multiplication(integers):
+    return int(integers[0]) * int(integers[1])
+
+
+def division(integers):
+    return int(integers[0]) / int(integers[1])
--- main.py
+++ main.py
@@ -1,20 +1,36 @@
-# from functions.py import *
+from functions import *
 
 def main():
     print("Use + for addition, Use - for subtraction, Use * for multiplication, Use / for division.")
     problem = input("What is the math problem to solve(one-step)? ")
-    valid = False
+    invalid = True
     functions = ["+", "-", "*", "/"]
+    operator = None
 
     for each in functions:
         if each in problem:
-            valid = True
+            operator = each
+            invalid = False
 
-    if not valid:
+    if invalid:
         print("FAIL")
         exit()
-
     print("PASS")
+
+    spaceless_problem = problem.replace(" ", "")
+
+    integers = spaceless_problem.split(operator)
+    print(integers)
+    if operator == "+":
+        print(addition(integers))
+    elif operator == "-":
+        print(subtraction(integers))
+    elif operator == "*":
+        print(multiplication(integers))
+    elif operator == "/":
+        print(division(integers))
+    else:
+        print("An error has occurred.")
 
 
 if __name__ == '__main__':
